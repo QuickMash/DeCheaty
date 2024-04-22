68a862b96e2dfd39ea42f89f86fa8a60e6555ee4
QuickMash <106212829+QuickMash@users.noreply.github.com>
2024-04-22 15:13:55 -0600

    Update getupdate.py

--- updater/getupdate.py
+++ updater/getupdate.py
@@ -1,12 +1,1 @@
-import requests
-# DO NOT CHANGE THIS
-version = '0.0.1'
-# Download location
-r = requests.get('http')
-print(version,'\nGetting Version...')
-print(r.status_code)
-if r.status_code == '200':
-  print('Connected!')
-# More code to download this github repo
-else:
-  print('Failed')
+# Updater IDK how to update
