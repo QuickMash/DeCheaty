2bc11feca9f4d6239392fb969017e75f13e8ad08
QuickMash <106212829+QuickMash@users.noreply.github.com>
2024-04-22 12:20:47 -0600

    Update install.py

---
+++ src/installer/install.py
@@ -0,0 +1,9 @@
+import system
+import reqfrom urllib.request import urlretrieve
+update_url = ''
+if system.system == 'Windows'
+elif system.system == 'Linux':
+elif system.system == ''
+else:
+  print('Downloading Generic')
+  urlretrieve()
